7a310a5f747ebbed099d2fcd77d99ea2e00b167c
Simon <rave.simon@live.fr>
2022-10-14 15:34:46 +0200

    main

--- main.py
+++ main.py
@@ -47,20 +47,39 @@ class Vehicule():
         print(f"kilometrage={self.km} vitesse_max={self.vmax} places={self.p}")
 
 
+
+class C:
+    nb_instances = 0
+
+    def __init__(self,x,y):
+      self.x = x
+      self.y = y
+      print(f"C({x},{y}) créé \t(#{id(self)})", end=" - ")
+      C.mod_instance(1)
+
+    def __del__(self):
+      print(f"C({self.x},{self.y}) détruit \t(#{id(self)})", end=" - ")
+      C.mod_instance(-1)
+
+    @classmethod
+    def mod_instance(cls,n):
+      cls.nb_instances += n
+      print(cls.how_many_instances())
+
+    @classmethod
+    def how_many_instances(cls):
+      return f"instances vivantes: {cls.nb_instances}"
+
+
 if __name__ == "__main__":
-    point = Pointeur(1,-1)
-    pointzoom = PointeurZoomable(0,0, 1)
-
-
-    localise(point)
-    avance(point, 9, 11)
-    localise(point)
-
-    localise(pointzoom)
-    avance(pointzoom, 9,11)
-    localise(pointzoom)
-    zoomer(pointzoom, 2)
-    localise(pointzoom)
+    z1 = C(1,2)
+    z2 = C(1,-2)
+    z1 = C(-1,-2)
+    del z2
+    z3 = z4 = C(0,0)
+    z5 = C(1,1)
+    del z4
+    print("fin de programme")
 
 
     
